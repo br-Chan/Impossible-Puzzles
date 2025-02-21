b625349fc9b33838e950196162e4aa40683d1bb8
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-02-22 10:00:13 +1300

    feat: create the 7 bridges of Konigsberg (pure CSS).

--- src/components/konigsberg/KonigsbergPuzzle.vue
+++ src/components/konigsberg/KonigsbergPuzzle.vue
@@ -5,10 +5,18 @@
         <h1>Each bridge is one crossing away from tumbling into the river rapids!</h1>
 
         <div class="box">
-            <div class="upper-river"></div>
-            <div class="left-river"></div>
-            <div class="right-river"></div>
-            <div class="lower-river"></div>
+            <div class="river upper-river"></div>
+            <div class="river left-river"></div>
+            <div class="river right-river"></div>
+            <div class="river lower-river"></div>
+
+            <div class="bridge bridge-1"></div>
+            <div class="bridge bridge-2"></div>
+            <div class="bridge bridge-3"></div>
+            <div class="bridge bridge-4"></div>
+            <div class="bridge bridge-5"></div>
+            <div class="bridge bridge-6"></div>
+            <div class="bridge bridge-7"></div>
         </div>
 
         <div class="prompt">
@@ -37,10 +45,10 @@
 
     --mask: linear-gradient(
             to right,
-            rgba(0, 0, 0, 0.1) 0,
+            rgba(0, 0, 0, 0) 0,
             rgba(0, 0, 0, 1) 10%,
             rgba(0, 0, 0, 1) 90%,
-            rgba(0, 0, 0, 0.1) 100%,
+            rgba(0, 0, 0, 0) 100%,
             rgba(0, 0, 0, 0) 0
         )
         100% 50% / 100% 100% repeat-x;
@@ -49,14 +57,17 @@
     mask: var(--mask);
 }
 
+.river {
+    background: cyan;
+}
+
 .upper-river {
     position: absolute;
     top: 20%;
     left: 20%;
 
-    width: 100%;
+    width: 80%;
     height: 1rem;
-    background: cyan;
 }
 
 .lower-river {
@@ -65,7 +76,6 @@
 
     width: 100%;
     height: 1rem;
-    background: cyan;
 }
 
 .left-river {
@@ -75,7 +85,6 @@
 
     width: 1rem;
     height: 65%;
-    background: cyan;
 }
 
 .right-river {
@@ -85,6 +94,62 @@
 
     width: 1rem;
     height: 65%;
-    background: cyan;
+}
+
+.bridge {
+    width: 1rem;
+    height: 2.5rem;
+    background-color: greenyellow;
+    border-left: 3px solid brown;
+    border-right: 3px solid brown;
+}
+
+.bridge-1 {
+    position: absolute;
+    top: 15%;
+    left: 30%;
+    transform: rotate(-3deg);
+}
+
+.bridge-2 {
+    position: absolute;
+    top: 17%;
+    right: 20%;
+    transform: rotate(5deg);
+}
+
+.bridge-3 {
+    position: absolute;
+    top: 45%;
+    left: 20%;
+    transform: rotate(90deg);
+}
+
+.bridge-4 {
+    position: absolute;
+    top: 47%;
+    right: 30%;
+    transform: rotate(90deg);
+}
+
+.bridge-5 {
+    position: absolute;
+    bottom: 10%;
+    left: 50%;
+    transform: rotate(-1deg);
+}
+
+.bridge-6 {
+    position: absolute;
+    bottom: 13%;
+    left: 25%;
+    transform: rotate(5deg);
+}
+
+.bridge-7 {
+    position: absolute;
+    bottom: 12%;
+    right: 17%;
+    transform: rotate(-10deg);
 }
 </style>
